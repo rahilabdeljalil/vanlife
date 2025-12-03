03366786b7c4aa423f74949f4e42b7e9826ef66d
rahilabdeljalil <rahilabdo17@gmail.com>
2025-12-03 17:34:06 +0100

    vans done

--- src/Vans.jsx
+++ src/Vans.jsx
@@ -1,24 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+
 export default function Vans() {
-  
-    const [vans, setVans] = useState([]);
-useEffect(() => {
+  const [vans, setVans] = useState([]);
+  useEffect(() => {
     fetch("/api/vans")
-        .then((res) => {
-            console.log("Fetch response:", res); // see if you get a response
-            return res.json();
-        })
-        .then((data) => {
-            console.log("Fetched data:", data); // see the actual data
-            setVans(data.vans);
-        })
-        .catch((err) => console.error("Fetch failed:", err)); // see errors
-}, []);
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans));
+  }, []);
 
+  const vanElements = vans.map((van) => (
+    <div key={van.id} className="van-tile">
+      <img src={van.imageUrl} />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>
+          ${van.price}
+          <span>/day</span>
+        </p>
+      </div>
+      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </div>
+  ));
 
-    return (
-        <div className="vans-container">
-           
-        </div>
-    );
+  return (
+    <div className="van-list-container">
+      <div className="van-list">{vanElements}</div>
+    </div>
+  );
 }
